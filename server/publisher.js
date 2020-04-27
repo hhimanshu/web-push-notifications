1ef5b1d6f6c6e9417aa9444563a9b9c2b70ff977
hhimanshu <harit.himanshu@yahoo.in>
2020-04-27 10:43:18 -0700

    05-04: Server Push working with single message

---
+++ server/publisher.js
@@ -0,0 +1,34 @@
+const webPush = require('web-push');
+
+const pushSubscription = {
+    "endpoint": "https://fcm.googleapis.com/fcm/send/cgpM2kf-o9I:APA91bGsFxoaA-YvOzeMUtmn3BIiWotLkpU3cwZNXOOBNGUJ9e1U6IIHd-oLnsgxFnW2GH6UnBeiXflxPUXmkBl_kk_vkL-oUHMFAOEwKUUsA4Zr9x8x6G9Weu9OnGIW94lJw5YEHYDc",
+    "expirationTime": null,
+    "keys": {
+        "p256dh": "BCUxEO-k-xwq8TzNYrvVbvMGjJS2tEQWfZCt6pvbN1OznFSxdTbFCGOgcNfpVWo2imU8ybRamYKz5xVUCJs43do",
+        "auth": "JutsLAPMx6UcqHQrRkm6JQ"
+    }
+}
+
+const vapidPublicKey = 'BBjDHF8cFoJznds83RrbZByg-UQCxWTNb9afVNjxjbpIsj2CaJyKDrndm9GwHk9Tbargfzt83gYprv9kl1OvIEU';
+const vapidPrivateKey = 'j1bKaA0HcYpB58PW3Ho2wg2WcK7raVplzN7zGo3Pnfw';
+
+const options = {
+    TTL: 60,
+    vapidDetails: {
+        subject: 'mailto: pushers@pushy.com',
+        publicKey: vapidPublicKey,
+        privateKey: vapidPrivateKey
+    }
+};
+
+const notify = () => {
+    webPush.sendNotification(
+            pushSubscription,
+            "Hello from Server!",
+            options
+        )
+        .then(() => console.log(`subscribers notified.`))
+        .catch(error => console.error('Error in pushing notification', error))
+}
+
+notify()
--- service-worker.js
+++ service-worker.js
@@ -4,11 +4,22 @@ self.addEventListener('notificationclose', event => {
 
 self.addEventListener('notificationclick', event => {
     if (event.action === "search") {
-          const githubUser = event.notification.data.githubUser;
-          clients.openWindow(`https://github.com/${githubUser}`);
+        const githubUser = event.notification.data.githubUser;
+        clients.openWindow(`https://github.com/${githubUser}`);
     } else if (event.action === "close") {
         clients.openWindow(`https://rebrand.ly/funny-dog`);
     }
 
     console.log('notification clicked', event)
+})
+
+self.addEventListener('push', event => {
+    const data = event.data.text();
+    const options = {
+        body: data
+    }
+
+    event.waitUntil(
+        self.registration.showNotification("Server Push", options)
+    )
 })
